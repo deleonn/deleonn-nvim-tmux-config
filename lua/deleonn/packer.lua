4ce2dea19205c3f9a108f8f0c606cd3b1bedfe8b
deleonn <joel.dl08@gmail.com>
2024-02-29 11:06:57 -0600

    update

--- lua/deleonn/packer.lua
+++ lua/deleonn/packer.lua
@@ -67,5 +67,7 @@ return require('packer').startup(function(use)
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }    
 
+    use('airblade/vim-gitgutter')
+
     use('~/Developer/vimplugins/todo.nvim')
 end)
